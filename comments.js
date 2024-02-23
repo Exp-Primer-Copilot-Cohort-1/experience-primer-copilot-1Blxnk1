a31bec8040198794b0408b3308f14f768e313ec8
Blxnk <66072729+1Blxnk1@users.noreply.github.com>
2024-02-23 14:48:58 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,84 +1,16 @@
-//creat web server
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var mime = require('mime');
-var comments = require('./comments');
-var cache = {};
+// Create web server
+// To run this file, you need to install express and body-parser
+// npm install express body-parser
 
-var server = http.createServer(function (request, response) {
-    var filePath = false;
-    if (request.url == '/') {
-        filePath = 'public/index.html';
-    } else {
-        filePath = 'public' + request.url;
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+// In-memory data store
+const comments = [
+    {
+        id: "1",
     }
-    var absPath = './' + filePath;
-    serveStatic(response, cache, absPath);
-});
-server.listen(3000, function () {
-    console.log("Server listening on port 3000.");
-});
-
-var chatServer = require('./lib/chat_server');
-chatServer.listen(server);
-
-function send404(response) {
-    response.writeHead(404, { 'Content-Type': 'text/plain' });
-    response.write('Error 404: resource not found.');
-    response.end();
-}
-
-function sendFile(response, filePath, fileContents) {
-    response.writeHead(
-        200,
-        { "content-type": mime.lookup(path.basename(filePath)) }
-    );
-    response.end(fileContents);
-}
-
-function serveStatic(response, cache, absPath) {
-    if (cache[absPath]) {
-        sendFile(response, absPath, cache[absPath]);
-    } else {
-        fs.exists(absPath, function (exists) {
-            if (exists) {
-                fs.readFile(absPath, function (err, data) {
-                    if (err) {
-                        send404(response);
-                    } else {
-                        cache[absPath] = data;
-                        sendFile(response, absPath, data);
-                    }
-                });
-            } else {
-                send404(response);
-            }
-        });
-    }
-}
-
-//Path: comments.js
-//create a socket.io server
-var socketio = require('socket.io');
-var io;
-var guestNumber = 1;
-var nickNames = {};
-var namesUsed = [];
-var currentRoom = {};
-
-exports.listen = function (server) {
-    io = socketio.listen(server);
-    io.set('log level', 1);
-    io.sockets.on('connection', function (socket) {
-        guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
-        joinRoom(socket, 'Lobby');
-        handleMessageBroadcasting(socket, nickNames);
-        handleNameChangeAttempts(socket, nickNames, namesUsed);
-        handleRoomJoining(socket);
-        socket.on('rooms', function () {
-            socket.emit('rooms', io.sockets.manager.rooms);
-        }
-        );
-    });
-}
+]; // Add closing square bracket
